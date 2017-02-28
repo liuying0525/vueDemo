6f819d6470a9723ec3ee1170d50afee49db4e8af
liuying0525 <181823455@qq.com>
2017-02-28 09:19:29 +0800

    first

---
+++ sell/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <div class="header">
+        I am header!
+    </div>
+    <div class="tab">
+      I am tab
+    </div>
+    <div class="content">
+      I am content
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
